fb263ebf0823a89f5096bd6e7ab68e6cbbe7fdaa
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2025-04-24 17:11:43 +0900

    25_4_24
    add branch MultiRendering
    add RenderingPipeline scripts

---
+++ EngineCode/Rendering/Core/DirectX/RenderingPipeline/RenderingPipeline.cpp
@@ -0,0 +1,6 @@
+#include "RenderingPipeline.h"
+
+FRenderingPipeline::FRenderingPipeline()
+{
+
+}
---
+++ EngineCode/Rendering/Core/DirectX/RenderingPipeline/RenderingPipeline.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../../../../Interface/DirectXDeviceInterfece.h"
+#include "Geometry/GeometryMap.h"
+#include "../../../../Shader/Core/Shader.h"
+#include "PipelineState/DirectXPipelineState.h"
+#include "RootSignature/DirectXRootSignature.h"
+#include "../../../../Core/Viewport/ViewportInfo.h"
+
+// 渲染流水线基类
+class GMesh; class FRenderingPipeline :public IDirectXDeviceInterface
+{
+
+};
