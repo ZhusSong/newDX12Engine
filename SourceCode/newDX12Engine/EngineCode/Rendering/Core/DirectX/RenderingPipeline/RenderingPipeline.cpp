1ba1102bcbc4e52086d9dd3bcf8baa3f62f6cd82
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2025-09-21 16:54:59 +0900

    25.9.21
    update

--- SourceCode/newDX12Engine/EngineCode/Rendering/Core/DirectX/RenderingPipeline/RenderingPipeline.cpp
+++ SourceCode/newDX12Engine/EngineCode/Rendering/Core/DirectX/RenderingPipeline/RenderingPipeline.cpp
@@ -35,19 +35,19 @@ void FRenderingPipeline::BuildPipeline()
 {																									//初始化GPS描述
 	DirectXPipelineState.ResetGPSDesc();
 
-	//渲染层级的初始化
+	// 渲染层级的初始化
 	RenderLayer.Init(&GeometryMap, &DirectXPipelineState);
 
-	//统一排序对渲染层级进行排序
+	// 统一排序对渲染层级进行排序
 	RenderLayer.Sort();
 
-	//读取贴图纹理
+	// 读取贴图纹理
 	GeometryMap.LoadTexture();
 
-	//构建雾气
+	// 构建雾气
 	GeometryMap.BuildFog();
 
-	//构建动态的CubeMap
+	// 构建动态的CubeMap
 	DynamicCubeMap.Init(
 		&GeometryMap,
 		&DirectXPipelineState,
@@ -72,22 +72,22 @@ void FRenderingPipeline::BuildPipeline()
 		&DirectXPipelineState,
 		&RenderLayer);
 
-	//构建根签名
+	// 构建根签名
 	RootSignature.BuildRootSignature(GeometryMap.GetDrawTexture2DResourcesNumber());
 	DirectXPipelineState.BindRootSignature(RootSignature.GetRootSignature());
 
-	//构建模型
+	// 构建模型
 	GeometryMap.Build();
 
-	//构建动态反射Mesh
+	// 构建动态反射Mesh
 	GeometryMap.BuildDynamicReflectionMesh();
 
-	//构建常量描述堆
+	// 构建常量描述堆
 	GeometryMap.BuildDescriptorHeap();
 
 	SSAO.BuildDescriptors();
 
-	//初始化我们的UI管线
+	// 初始化UI管线
 	UIPipeline.Init(
 		GeometryMap.GetHeap(),
 		GeometryMap.GetDrawTexture2DResourcesNumber() + //Texture2D
@@ -96,46 +96,46 @@ void FRenderingPipeline::BuildPipeline()
 		1 + //Shadow
 		1);//ShadowCubeMap
 
-	//初始化CubeMap 摄像机
+	// 初始化CubeMap 摄像机
 	DynamicCubeMap.BuildViewport(fvector_3d(0.f, 0.f, 0.f));
 
-	//构建深度模板描述
+	// 构建深度模板描述
 	DynamicCubeMap.BuildDepthStencilDescriptor();
 
-	//构建RTVDes
+	// 构建RTVDes
 	DynamicCubeMap.BuildRenderTargetDescriptor();
 
-	//构建深度模板
+	// 构建深度模板
 	DynamicCubeMap.BuildDepthStencil();
 
-	//构建阴影
+	// 构建阴影
 	GeometryMap.BuildShadow();
 
-	//构建常量缓冲区
+	// 构建常量缓冲区
 	GeometryMap.BuildMeshConstantBuffer();
 
-	//构建材质常量缓冲区
+	// 构建材质常量缓冲区
 	GeometryMap.BuildMaterialShaderResourceView();
 
-	//构建灯光常量缓冲区
+	// 构建灯光常量缓冲区
 	GeometryMap.BuildLightConstantBuffer();
 
-	//构建我们的视口常量缓冲区视图
+	// 构建视口常量缓冲区视图
 	GeometryMap.BuildViewportConstantBufferView();
 
-	//构建贴图
+	// 构建贴图
 	GeometryMap.BuildTextureConstantBuffer();
 
-	//构建雾气常量缓冲区
+	// 构建雾气常量缓冲区
 	GeometryMap.BuildFogConstantBuffer();
 
-	//构建SSAO
+	// 构建SSAO
 	SSAO.Build();
 
-	//存储一个默认的GPS描述数据
+	// 存储一个默认的GPS描述数据
 	DirectXPipelineState.SaveGPSDescAsDefault();
 
-	//通过层级来构建PSO
+	// 通过层级来构建PSO
 	RenderLayer.BuildPSO();
 }
 
@@ -147,29 +147,29 @@ void FRenderingPipeline::PreDraw(float DeltaTime)
 	GeometryMap.PreDraw(DeltaTime);
 	RootSignature.PreDraw(DeltaTime);
 
-	//渲染灯光材质贴图等(必须要放在 这个位置 否则天崩地裂)
+	// 渲染灯光材质贴图等
 	GeometryMap.Draw(DeltaTime);
 
-	//渲染SSAO
-	SSAO.Draw(DeltaTime);
+	// 渲染SSAO
+	// SSAO.Draw(DeltaTime);
 	RootSignature.PreDraw(DeltaTime);
 
-	//存储我们的SSAO到指定的buffer
+	// 存储我们的SSAO到指定的buffer
 	SSAO.SaveToSSAOBuffer();
 
-	//主视口清除画布
+	// 主视口清除画布
 	ClearMainSwapChainCanvas();
 
-	//重新绑定贴图
+	// 重新绑定贴图
 	GeometryMap.Draw2DTexture(DeltaTime);
 
-	//渲染
+	// 渲染
 	GeometryMap.DynamicShadowCubeMap.PreDraw(DeltaTime);
 
-	//渲染阴影
+	// 渲染阴影
 	GeometryMap.DrawShadow(DeltaTime);
 
-	//动态反射
+	// 动态反射
 	if (DynamicCubeMap.IsExitDynamicReflectionMesh())
 	{
 		DynamicCubeMap.PreDraw(DeltaTime);
@@ -180,27 +180,27 @@ void FRenderingPipeline::PreDraw(float DeltaTime)
 
 void FRenderingPipeline::Draw(float DeltaTime)
 {
-	//主视口
+	// 主视口
 	GeometryMap.DrawViewport(DeltaTime);
 
-	//CubeMap 覆盖原先被修改的CubeMap
+	// CubeMap 覆盖原先被修改的CubeMap
 	GeometryMap.DrawCubeMapTexture(DeltaTime);
 
-	//各类层级
+	// 各类层级
 	RenderLayer.Draw(RENDERLAYER_BACKGROUND, DeltaTime);
 	RenderLayer.Draw(RENDERLAYER_OPAQUE, DeltaTime);
 	RenderLayer.Draw(RENDERLAYER_TRANSPARENT, DeltaTime);
 
-	//渲染选择层
+	// 渲染选择层
 	RenderLayer.Draw(RENDERLAYER_SELECT, DeltaTime);
 
-	//渲染旋转面片
+	// 渲染旋转面片
 	RenderLayer.Draw(RENDERLAYER_OPERATION_HANDLE_ROT_PLANE, DeltaTime);
 
-	//渲染操作手柄
+	// 渲染操作手柄
 	RenderLayer.Draw(RENDERLAYER_OPERATION_HANDLE, DeltaTime);
 
-	//渲染UI
+	// 渲染UI
 	UIPipeline.Draw(DeltaTime);
 
 	DirectXPipelineState.Draw(DeltaTime);
